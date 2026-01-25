a656ef2055af390aa89d93e3712f3e293030b57e
khoaminh432 <khoakutip123@gmail.com>
2026-01-25 22:58:53 +0700

    add function for product

---
+++ src/main/java/my_app/controller/component/product/handleListController.java
@@ -0,0 +1,33 @@
+package my_app.controller.component.product;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import my_app.model.Product;
+public class handleListController {
+     @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private Button btnView;
+
+    private Product product;
+
+    public void setData(Product product){
+        this.product = product;
+    }
+    @FXML
+    private void handleEdit(){
+        System.out.println("Edit product: " + product.getProductName());
+    }
+    @FXML
+    private void handleDelete(){
+        System.out.println("Delete product: " + product.getProductName());
+    }
+    @FXML
+    private void handleView(){
+        System.out.println("View product: " + product.getProductName());
+    }
+}
--- src/main/java/my_app/controller/indexController.java
+++ src/main/java/my_app/controller/indexController.java
@@ -1,18 +1,24 @@
 package my_app.controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import my_app.service.LoadFileGUI;
 import my_app.service.SwitchPage;
 public class indexController extends SwitchPage {
 
+    private final LocalDate dateNow = LocalDate.now();
     @FXML
     private VBox mainVBox;
+
+    @FXML
+    private Label lbTimeNow;
 
     @FXML
     private AnchorPane apLoadPane;
@@ -35,9 +41,11 @@ public class indexController extends SwitchPage {
     @FXML
     private Button btnSupplierPage;
 
+    
+
     private LoadFileGUI HomePage ;
     private LoadFileGUI ProductPage ;
-
+    private LoadFileGUI CustomerPage ;
     private void switchPage(Parent page){
         
         SwitchPage(apLoadPane, page);
@@ -62,11 +70,21 @@ public class indexController extends SwitchPage {
     }
 
     @FXML
+    private void loadCustomerPage() throws IOException{
+        System.out.println("Load Customer Page");
+        CustomerPage = createPage("/fxml/admin/component/customerpage.fxml");
+        loadPage(CustomerPage.load());
+    }
+
+    private void loadLabel(){
+        lbTimeNow.setText(dateNow.toString());
+    }
+
+    @FXML
     public void initialize() {
         try {
             loadHomePage();
-            loadProductPage();
-            loadHomePage();
+            loadLabel();
         } catch (IOException e) {
             e.printStackTrace();
         }
