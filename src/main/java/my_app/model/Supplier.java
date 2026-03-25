e063a02483257ebe705d954b020d5611f4a94650
khoaminh432 <khoakutip123@gmail.com>
2026-03-25 12:49:28 +0700

    add phieu nhap hang

--- src/main/java/my_app/model/Supplier.java
+++ src/main/java/my_app/model/Supplier.java
@@ -3,14 +3,16 @@ package my_app.model;
 import java.util.Map;
 
 public class Supplier {
+
     private Integer id;
     private String supplierName;
     private String address;
     private String phoneNumber;
-    
+
     // Constructors
-    public Supplier() {}
-    
+    public Supplier() {
+    }
+
     public Supplier(Integer id, String supplierName, String address, String phoneNumber) {
         this.id = id;
         this.supplierName = supplierName;
@@ -25,9 +27,11 @@ public class Supplier {
     public Supplier(Supplier other) {
         this(other.id, other.supplierName, other.address, other.phoneNumber);
     }
+
     public Supplier(Map<String, Object> data) {
         applyFromMap(data);
     }
+
     public void applyFromMap(Map<String, Object> data) {
         if (data == null || data.isEmpty()) {
             return;
@@ -53,27 +57,42 @@ public class Supplier {
             this.phoneNumber = newPhoneNumber;
         }
     }
-    
+
     // Getters and Setters
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-    
-    public String getSupplierName() { return supplierName; }
-    public void setSupplierName(String supplierName) { this.supplierName = supplierName; }
-    
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-    
-    public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     @Override
     public String toString() {
-        return "Supplier{" +
-               "id=" + id +
-               ", supplierName='" + supplierName + '\'' +
-               ", address='" + address + '\'' +
-               ", phoneNumber='" + phoneNumber + '\'' +
-               '}';
+        return supplierName;
     }
 }
